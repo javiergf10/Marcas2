2c2a4180b61ae929c8451f374c087701b5cccbfa
javiergf10 <javiergf10@educastur.es>
2023-04-14 17:00:02 +0200

    1

---
+++ javascrip/Examen 2a/formulario.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<?php echo $_GET['nombre']?>
+<?php $nombre = $_POST['nombre'];?>
+<?php $apellidos = $_POST['apellidos']?>
+<?php echo $nombre;?>
+<?php echo $apellidos;?>
+
+</body>
+</html>
